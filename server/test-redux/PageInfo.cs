bdbf7ca38ceb71f737cb309dea13d6b40319b173
Dan Nguyen <ddnguyen0702@hotmail.com>
2018-02-04 16:03:09 +0700

    update

---
+++ server/test-redux/PageInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace test_redux
+{
+	public class PagedList<T>
+	{
+		[JsonProperty(PropertyName = "list")]
+		public IEnumerable<T> List { get; set; }
+
+		[JsonProperty(PropertyName = "page")]
+		public int Page { get; set; }
+
+		[JsonProperty(PropertyName = "size")]
+		public int Size { get; set; }
+
+		[JsonProperty(PropertyName = "count")]
+		public long Count { get; set; }
+	}
+}
