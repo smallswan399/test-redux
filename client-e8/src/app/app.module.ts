ed9254e0362085aabb04229f7495fd543a011dab
user <user@user.com>
2019-11-25 02:28:06 +0700

    x

--- client-e8/src/app/app.module.ts
+++ client-e8/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgReduxModule, NgRedux, DevToolsExtension } from '@angular-redux/store';
 
 import { AppComponent } from './app.component';
 
@@ -13,4 +14,12 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<IAppState>, private devTools: DevToolsExtension) {
+      let enhancers = [];
+      if (devTools.isEnabled()) {
+          enhancers = [...enhancers, devTools.enhancer()];
+        }
+      ngRedux.configureStore(rootReducer, INITIAL_STATE, [], enhancers);
+  }
+}
